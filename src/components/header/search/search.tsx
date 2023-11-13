d99ad7101e9bb6bbbfdb634802f8665ebe157b0c
Shaheer <shaheer@deriv.com>
2023-11-13 09:10:12 +0400

    feat: :boat: adds search input

---
+++ src/components/header/search/search.tsx
@@ -0,0 +1,11 @@
+const Search = () => (
+  <div className='flex w-full items-center justify-center'>
+    <input
+      className='rounded-xl border px-4 py-2 shadow-xl outline-none transition-all delay-100 focus:border-2 focus:border-rose-500 md:min-w-[20rem] '
+      type='text'
+      placeholder='Search'
+    />
+  </div>
+);
+
+export default Search;
