27b3e1220b938267726e3a66475b4e918d80a71c
Shaheer <shaheer@deriv.com>
2023-11-13 08:32:02 +0400

    feat: :rainbow: adds deriv header logo

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,9 +1,11 @@
+import { BrandDerivLogoCoralIcon } from '@deriv/quill-icons/Logo';
+
 const Header = () => (
   <div className='sticky top-0 flex h-20 items-center bg-white px-8 shadow-sm'>
     <div className='grid w-full grid-cols-[1fr_2fr_1fr]'>
       <div className='flex items-center gap-2 font-bold'>
-        <div>Deriv Logo</div>
-        <span>Quill Icons</span>
+        <BrandDerivLogoCoralIcon width={32} />
+        <span className='font-bold'>Quill Icons</span>
       </div>
       <div>Search</div>
       <div />
