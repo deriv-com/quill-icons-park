294a5394428e692896d2c5d9b030749dd4f5916d
Shaheer <shaheer@deriv.com>
2023-11-13 09:30:44 +0400

    feat: :classical_building: adds responsiveness to header

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,13 +1,14 @@
 import { BrandDerivLogoCoralIcon } from '@deriv/quill-icons/Logo';
+import Search from './search/search';
 
 const Header = () => (
-  <div className='sticky top-0 flex h-20 items-center bg-white px-8 shadow-sm'>
-    <div className='grid w-full grid-cols-[1fr_2fr_1fr]'>
-      <div className='flex items-center gap-2 font-bold'>
+  <div className='sticky top-0 flex h-28 items-center bg-white px-8 shadow-sm sm:h-20'>
+    <div className='grid w-full gap-2 sm:grid-cols-[1fr_2fr_1fr]'>
+      <div className='mt-2 flex items-center justify-center gap-2 font-bold sm:mt-0'>
         <BrandDerivLogoCoralIcon width={32} />
         <span className='font-bold'>Quill Icons</span>
       </div>
-      <div>Search</div>
+      <Search />
       <div />
     </div>
   </div>
--- src/components/header/search/search.tsx
+++ src/components/header/search/search.tsx
@@ -1,7 +1,7 @@
 const Search = () => (
   <div className='flex w-full items-center justify-center'>
     <input
-      className='rounded-xl border px-4 py-2 shadow-xl outline-none transition-all delay-100 focus:border-2 focus:border-rose-500 md:min-w-[20rem] '
+      className='rounded-xl border-2 border-transparent px-4 py-2 shadow-xl outline-none transition-all delay-75 focus:border-rose-500 sm:min-w-[20rem] '
       type='text'
       placeholder='Search'
     />
