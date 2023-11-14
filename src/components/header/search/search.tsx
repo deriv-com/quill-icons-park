6f80c45c0b8cc7a2c01916bd21d4e916bca0879c
Shaheer <shaheer@deriv.com>
2023-11-14 20:23:19 +0400

    feat: :closed_umbrella: integrates search functionality

--- src/components/header/search/search.tsx
+++ src/components/header/search/search.tsx
@@ -1,11 +1,20 @@
-const Search = () => (
-  <div className='flex w-full items-center justify-center'>
-    <input
-      className='rounded-xl border-2 border-transparent px-4 py-2 shadow-xl outline-none transition-all delay-75 focus:border-rose-500 sm:min-w-[20rem] '
-      type='text'
-      placeholder='Search'
-    />
-  </div>
-);
+import { useContext } from 'react';
+import { SearchContext } from '../../../context/search_context';
+
+const Search = () => {
+  const searchContext = useContext(SearchContext);
+  const setSearchText = searchContext?.setSearchText;
+
+  return (
+    <div className='flex w-full items-center justify-center'>
+      <input
+        className='rounded-xl border-2 border-transparent px-4 py-2 shadow-xl outline-none transition-all delay-75 focus:border-rose-500 sm:min-w-[20rem] '
+        type='text'
+        onChange={(event) => setSearchText?.(event.target.value)}
+        placeholder='Search'
+      />
+    </div>
+  );
+};
 
 export default Search;
