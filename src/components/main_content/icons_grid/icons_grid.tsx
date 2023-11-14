0e0b3532c44744fa43ab35f270307ac322e0a910
Shaheer <shaheer@deriv.com>
2023-11-14 18:47:25 +0400

    feat: :grapes: integrates quill-icons via plain imports

--- src/components/main_content/icons_grid/icons_grid.tsx
+++ src/components/main_content/icons_grid/icons_grid.tsx
@@ -1,8 +1,13 @@
+import { useCallback } from 'react';
 import { getIcons } from '../../../utils/icon_utils';
 import IconEntry from './icon_entry/icon_entry';
+import { CATEGORIES } from '../../../constants/categories';
 
 const IconsGrid = () => {
-  const icons = getIcons();
+  const category = localStorage.getItem('category') ?? CATEGORIES.ALL;
+
+  const memoizedGetIcons = useCallback(() => getIcons(category), [category]);
+  const icons = memoizedGetIcons();
 
   return (
     <div className='grid grid-cols-3 gap-4 p-4 md:grid-cols-6'>
