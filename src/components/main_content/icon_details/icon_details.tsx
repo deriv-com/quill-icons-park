7e4f91f10d1002ee6c6370c4a89451225ddcbb5f
Shaheer <shaheer@deriv.com>
2023-11-16 17:35:50 +0400

    feat: :hamburger: adds visual info for no icon selected

--- src/components/main_content/icon_details/icon_details.tsx
+++ src/components/main_content/icon_details/icon_details.tsx
@@ -2,6 +2,7 @@ import { useContext, useEffect } from 'react';
 import { IconContext } from '../../../context/icon_context';
 import { CategoryContext } from '../../../context/category_context';
 import classNames from 'classnames';
+import { StandaloneComputerMouseScrollwheelBoldIcon } from '@deriv/quill-icons/Standalone';
 
 const IconDetails = () => {
   const iconContext = useContext(IconContext);
@@ -16,15 +17,26 @@ const IconDetails = () => {
   useEffect(() => setIconSelected?.(undefined), [categorySelected, setIconSelected]);
 
   return (
-    <div className='p-4'>
+    <div className='relative p-4'>
       <div
         className={classNames(
-          'sticky top-36 flex flex-col gap-4 rounded-xl p-4 shadow-xl transition-transform delay-100',
-          Icon ? 'translate-x-0' : 'translate-x-96',
+          'sticky top-36 flex flex-col gap-4 rounded-xl bg-white p-4 shadow-xl transition-all delay-100',
+          Icon ? 'opacity-1 translate-x-0' : 'translate-x-96 opacity-0',
         )}
       >
         <span>Icon Details</span>
         <div className='flex items-center justify-center'>{Icon && <Icon />}</div>
+      </div>
+      <div
+        className={classNames(
+          'sticky top-60 transition-opacity delay-100',
+          Icon ? 'translate-x-96 opacity-0' : 'translate-x-0 opacity-100',
+        )}
+      >
+        <div className='flex flex-col align-middle opacity-50'>
+          <StandaloneComputerMouseScrollwheelBoldIcon />
+          <span>Select an icon to see details!</span>
+        </div>
       </div>
     </div>
   );
