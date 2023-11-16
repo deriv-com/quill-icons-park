310ee85527f4db920fed4d4d05f30617a28eaf21
Shaheer <shaheer@deriv.com>
2023-11-16 19:06:50 +0400

    feat: :shower: adds visual search icon for the input field

--- src/components/header/search/search.tsx
+++ src/components/header/search/search.tsx
@@ -1,5 +1,6 @@
 import { useContext } from 'react';
 import { SearchContext } from '../../../context/search_context';
+import { StandaloneSearchBoldIcon } from '@deriv/quill-icons';
 
 const Search = () => {
   const searchContext = useContext(SearchContext);
@@ -7,12 +8,17 @@ const Search = () => {
 
   return (
     <div className='flex w-full items-center justify-center'>
-      <input
-        className='rounded-xl border-2 border-transparent px-4 py-2 shadow-xl outline-none transition-all delay-75 focus:border-rose-500 sm:min-w-[20rem] '
-        type='text'
-        onChange={(event) => setSearchText?.(event.target.value)}
-        placeholder='Search'
-      />
+      <span className='relative flex items-center justify-center'>
+        <input
+          className='rounded-xl border-2 border-transparent px-4 py-2 shadow-xl outline-none transition-all delay-75 focus:border-rose-500 sm:min-w-[20rem] '
+          type='text'
+          onChange={(event) => setSearchText?.(event.target.value)}
+          placeholder='Search'
+        />
+        <span className='absolute right-2 opacity-50'>
+          <StandaloneSearchBoldIcon />
+        </span>
+      </span>
     </div>
   );
 };
