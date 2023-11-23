64a783afa1202c605c7986277c4a9e61e41b5b31
Shaheer <shaheer@deriv.com>
2023-11-23 18:36:18 +0400

    feat: :pear: adds predefined and custom icon sizes option

--- src/components/main_content/icon_details/icon_size_selection/icon_size_selection.tsx
+++ src/components/main_content/icon_details/icon_size_selection/icon_size_selection.tsx
@@ -1,5 +1,12 @@
 import { IconSize } from '@deriv/quill-icons';
 import PredefinedIconSizeSelection from './predefined-icon-size-selection/predefined-icon-size-selection';
+import CustomIconSizeSelection from './custom-icon-size-selection/custom-icon-size-selection';
+import { useContext } from 'react';
+import { CategoryContext } from '../../../../context/category_context';
+import {
+  CUSTOM_ICON_SIZE_SELECTION_CATEGORIES,
+  PREDEFINED_ICON_SIZE_SELECTION_CATEGORIES,
+} from '../../../../constants/category_constants';
 
 type TIconSizeSelection = {
   iconSize: IconSize;
@@ -7,10 +14,18 @@ type TIconSizeSelection = {
 };
 
 const IconSizeSelection = ({ iconSize, setIconSize }: TIconSizeSelection) => {
+  const categoryContext = useContext(CategoryContext);
+  const categorySelected = categoryContext?.categorySelected;
+
   return (
     <div className='flex flex-col gap-2'>
       <span className='font-bold'>Icon Size</span>
-      <PredefinedIconSizeSelection iconSize={iconSize} setIconSize={setIconSize} />
+      {CUSTOM_ICON_SIZE_SELECTION_CATEGORIES.includes(categorySelected as string) && (
+        <CustomIconSizeSelection iconSize={iconSize} setIconSize={setIconSize} />
+      )}
+      {PREDEFINED_ICON_SIZE_SELECTION_CATEGORIES.includes(categorySelected as string) && (
+        <PredefinedIconSizeSelection iconSize={iconSize} setIconSize={setIconSize} />
+      )}
     </div>
   );
 };
