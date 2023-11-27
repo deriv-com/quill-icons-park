009262f4d7ab945de59fb7680e8cf67647be0215
Shaheer <shaheer@deriv.com>
2023-11-27 20:08:06 +0400

    style: :sandal: fixes icon display height in filtered view

--- src/components/main_content/icon_details/icon_size_selection/predefined-icon-size-selection/predefined-icon-size-selection.tsx
+++ src/components/main_content/icon_details/icon_size_selection/predefined-icon-size-selection/predefined-icon-size-selection.tsx
@@ -9,23 +9,28 @@ type TPredefinedIconSelection = {
 const PredefinedIconSizeSelection = ({
   predefinedIconSize,
   setPredefinedIconSize,
-}: TPredefinedIconSelection) => (
-  <div className='flex justify-between'>
-    {Object.keys(sizes).map((size) => (
-      <span
-        key={size}
-        onClick={() => setPredefinedIconSize(size as IconSize)}
-        className={classNames(
-          'flex min-w-[4rem] cursor-pointer items-center justify-center rounded-md border-2 px-2 py-1 shadow-md',
-          predefinedIconSize === size
-            ? 'cursor-default border-rose-500'
-            : 'border-slate-50 hover:border-slate-300',
-        )}
-      >
-        {size}
-      </span>
-    ))}
-  </div>
-);
+}: TPredefinedIconSelection) => {
+  const handleSizeSelection = (size: IconSize) => setPredefinedIconSize(size);
+
+  return (
+    <div className='flex justify-between'>
+      {Object.keys(sizes).map((size) => (
+        <span
+          key={size}
+          onKeyDown={() => handleSizeSelection(size as IconSize)}
+          onClick={() => handleSizeSelection(size as IconSize)}
+          className={classNames(
+            'flex min-w-[4rem] items-center justify-center rounded-md border-2 px-2 py-1 shadow-md',
+            predefinedIconSize === size
+              ? 'cursor-default border-rose-500'
+              : 'cursor-pointer border-slate-50 hover:border-slate-300',
+          )}
+        >
+          {size}
+        </span>
+      ))}
+    </div>
+  );
+};
 
 export default PredefinedIconSizeSelection;
--- src/components/main_content/icons_grid/icon_entry/icon_entry.tsx
+++ src/components/main_content/icons_grid/icon_entry/icon_entry.tsx
@@ -19,10 +19,10 @@ const IconEntry = ({ iconName, Icon }: TIconEntry) => {
       onKeyDown={undefined}
       onClick={() => setIconSelected?.({ Icon, iconName })}
       className={classNames(
-        'flex cursor-pointer flex-col justify-between overflow-hidden rounded-xl border-2 p-4 shadow-md',
+        'flex h-40 flex-col justify-between overflow-hidden rounded-xl border-2 p-4 shadow-md',
         isIconSelected
           ? 'cursor-default border-rose-500 bg-rose-50'
-          : 'border-transparent hover:border-slate-400 hover:bg-slate-50',
+          : 'cursor-pointer border-transparent hover:border-slate-400 hover:bg-slate-50',
       )}
     >
       <span />
