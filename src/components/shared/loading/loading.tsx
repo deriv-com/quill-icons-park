19ada414d8db82eebaa3349c677968a604ca0b7f
Shaheer <shaheer@deriv.com>
2023-11-30 18:42:11 +0400

    feat: :envelope: adds loading component

---
+++ src/components/shared/loading/loading.tsx
@@ -0,0 +1,10 @@
+import { IllustrativeFastTransactionIcon } from '@deriv/quill-icons/Illustrative';
+
+const Loading = () => (
+  <div className='text-md flex w-full flex-col items-center justify-center gap-2 p-4'>
+    <IllustrativeFastTransactionIcon />
+    <span>Loading...</span>
+  </div>
+);
+
+export default Loading;
