54381e5afa87a0ef32a7816ed4339e0718ebe065
Shaheer <shaheer@deriv.com>
2023-12-06 18:22:11 +0400

    refactor: :hatched_chick: renames folder

--- src/components/main_content/icons_grid/icons_grid.tsx
+++ src/components/main_content/icons_grid/icons_grid.tsx
@@ -3,7 +3,7 @@ import { CATEGORIES, DEFAULT_CATEGORY } from '../../../constants/category_consta
 import { CategoryContext } from '../../../context_provider/category_context';
 import { SearchContext } from '../../../context_provider/search_context';
 import NoIconsFound from './no_icons_found/no_icons_found';
-import useQuillIcons from '../../../hooks/quill_icons_hook/useQuillIcons';
+import useQuillIcons from '../../../hooks/quill_icons/useQuillIcons';
 import Loading from '../../shared/loading/loading';
 import IconCategory from './icon_category/icon_category';
 
