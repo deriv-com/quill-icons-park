592fec1c55206e8c1b9cbbd492b2bef0959002f8
Shaheer <shaheer@deriv.com>
2024-01-16 15:32:13 +0400

    feat: :zap: adds new platforms category

--- src/constants/category_constants.ts
+++ src/constants/category_constants.ts
@@ -9,6 +9,7 @@ export const CATEGORIES = Object.freeze({
   LABEL_PAIRED: 'Label Paired',
   LOGO: 'Logo',
   MARKETS: 'Markets',
+  PLATFORMS: 'Platforms',
   SOCIAL: 'Social',
   STANDALONE: 'Standalone',
 });
@@ -28,6 +29,7 @@ export const CATEGORIES_TYPE_B: string[] = [
   CATEGORIES.ILLUSTRATIVE,
   CATEGORIES.LABEL_PAIRED,
   CATEGORIES.MARKETS,
+  CATEGORIES.PLATFORMS,
   CATEGORIES.SOCIAL,
   CATEGORIES.STANDALONE,
 ];
@@ -40,6 +42,7 @@ export const CATEGORY_PROMISES = {
   [CATEGORIES.LABEL_PAIRED]: import('@deriv/quill-icons/LabelPaired'),
   [CATEGORIES.LOGO]: import('@deriv/quill-icons/Logo'),
   [CATEGORIES.MARKETS]: import('@deriv/quill-icons/Markets'),
+  [CATEGORIES.PLATFORMS]: import('@deriv/quill-icons/Platforms'),
   [CATEGORIES.SOCIAL]: import('@deriv/quill-icons/Social'),
   [CATEGORIES.STANDALONE]: import('@deriv/quill-icons/Standalone'),
 };
