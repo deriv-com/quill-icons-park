24e551abc42429aa81b194e67b266c8a04a4698c
Niloofar <niloofar.sadeghi@regentmarkets.com>
2024-04-16 15:09:44 +0800

    feat: added legacy icons

--- src/constants/category_constants.ts
+++ src/constants/category_constants.ts
@@ -13,6 +13,7 @@ export const CATEGORIES = Object.freeze({
   SOCIAL: 'Social',
   STANDALONE: 'Standalone',
   PAYMENTMETHODS: 'PaymentMethods',
+  LEGACY: 'Legacy',
 });
 
 export const CATEGORY_PROMISES = {
@@ -23,6 +24,7 @@ export const CATEGORY_PROMISES = {
   [CATEGORIES.ILLUSTRATIVE]: import('@deriv/quill-icons/Illustrative'),
   [CATEGORIES.LABEL_PAIRED]: import('@deriv/quill-icons/LabelPaired'),
   [CATEGORIES.LOGO]: import('@deriv/quill-icons/Logo'),
+  [CATEGORIES.LEGACY]: import('@deriv/quill-icons/Legacy'),
   [CATEGORIES.MARKETS]: import('@deriv/quill-icons/Markets'),
   [CATEGORIES.SOCIAL]: import('@deriv/quill-icons/Social'),
   [CATEGORIES.STANDALONE]: import('@deriv/quill-icons/Standalone'),
@@ -52,4 +54,5 @@ export const CATEGORIES_TYPE_B: string[] = [
   CATEGORIES.SOCIAL,
   CATEGORIES.STANDALONE,
   CATEGORIES.PAYMENTMETHODS,
+  CATEGORIES.LEGACY,
 ];
